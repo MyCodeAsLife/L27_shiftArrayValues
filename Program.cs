db8a1571e96bdb0d8507354f1fd6fdb6efec11c7
Vladimir <w112vovan@mail.ru>
2023-11-29 20:38:43 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L27_shiftArrayValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mainArray = { 1, 2, 3, 4, 5, 6, 7 };
+            int shiftLength;
+
+            Console.Write("На сколько порядков нужно сдвинуть массив: ");
+            shiftLength = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nМассив до сдвига:");
+
+            foreach (var item in mainArray)
+                Console.Write(item + " ");
+
+            for (int i = 0; i < shiftLength; i++)
+            {
+                int tempNumber = 0;
+
+                for (int j = 0; j < mainArray.Length; j++)
+                {
+                    if (j == 0)
+                    {
+                        tempNumber = mainArray[j + 1];
+                        mainArray[j + 1] = mainArray[j];
+                    }
+                    else if (j < mainArray.Length - 1)
+                    {
+                        tempNumber += mainArray[j + 1];
+                        mainArray[j + 1] = tempNumber - mainArray[j + 1];
+                        tempNumber -= mainArray[j + 1];
+                    }
+                    else if (j == mainArray.Length - 1)
+                    {
+                        mainArray[0] = tempNumber;
+                    }
+                }
+            }
+
+            Console.WriteLine($"\n\nМассив после сдвига на {shiftLength} порядков в право.");
+
+            foreach (var item in mainArray)
+                Console.Write(item + " ");
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("L27_shiftArrayValues")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("L27_shiftArrayValues")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("4edb47ad-cb86-4a79-a8b7-70ddaba38b11")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
